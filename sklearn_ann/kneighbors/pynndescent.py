7e0a0cf852240a21b0c5fd23d8ddf0c8b85d7d8e
Frankie Robertson <frankie@robertson.name>
2021-01-05 20:18:50 +0200

    Add workaround for PyNNDescentTransformer pickling problems

--- sklearn_ann/kneighbors/pynndescent.py
+++ sklearn_ann/kneighbors/pynndescent.py
@@ -1,4 +1,15 @@
-from pynndescent import PyNNDescentTransformer
+from pynndescent import PyNNDescentTransformer as PyNNDescentTransformerBase
+
+
+def no_op():
+    pass
+
+
+class PyNNDescentTransformer(PyNNDescentTransformerBase):
+    def fit(self, X, compress_index=True):
+        super().fit(X, compress_index=compress_index)
+        self.index_.compress_index = no_op
+        return self
 
 
 __all__ = ["PyNNDescentTransformer"]
