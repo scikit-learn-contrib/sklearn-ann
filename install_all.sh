ad7ab2e58f0c3268bf1b081bef639ddbac14bd47
Frankie Robertson <frankie@robertson.name>
2023-06-29 16:29:33 +0300

    Use nmslib from pypi

--- install_all.sh
+++
@@ -1,2 +0,0 @@
-poetry install -E tests -E docs -E annoy -E faiss -E pynndescent
-poetry run pip install "git+https://github.com/nmslib/nmslib.git@fd969978ad49a7135b1a153826b5c460dc53d0ba#egg=nmslib&subdirectory=python_bindings"
